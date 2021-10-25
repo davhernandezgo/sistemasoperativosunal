201d1c3b62c9894f156e8fbb8335be7d10a5904f
davhernandezgo <92111551+davhernandezgo@users.noreply.github.com>
2021-10-25 15:57:21 -0500

    Add files via upload

---
+++ llamadosalsistema/Ej5.py
@@ -0,0 +1,3 @@
+# Retorna el nombre del usuario que inició sesión en el terminal que controla el proceso.
+import os
+print (os.getlogin())
---
+++ llamadosalsistema/Ej6.py
@@ -0,0 +1,3 @@
+# True si el tipo de entorno nativo del sistema operativo es bytes.
+import os
+print (os.supports_bytes_environ)
