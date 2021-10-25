7dda6160802a8a328e1efe1c41aefc18b4fb163a
davhernandezgo <92111551+davhernandezgo@users.noreply.github.com>
2021-10-25 17:13:22 -0500

    Delete Ej10.py

--- llamadosalsistema/Ej10.py
+++
@@ -1,7 +0,0 @@
-# se utilizan para referirse a los directorios actuales y principales de manera portable.
-import os
-
-print('Starting:', os.getcwd())
-print('Moving up one:', os.pardir)
-os.chdir(os.pardir)
-print('After move:', os.getcwd())
